cb090eb36a27027a3a1bd932e612fd4ddac093fc
parinya.c <parinya.c@carpass.co.th>
2012-01-30 16:49:15 +0700

    add jQuery effect, fade[In, Out, Toggle], slide[Up, Down, Toggle] and animate
    Signed-off-by: parinya.c <parinya.c@carpass.co.th>

---
+++ html5-javascript-animation/TheAnimation/TheAnimation/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheAnimation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult jQueryEffect1()
+        {
+            return View();
+        }
+
+        public ActionResult changeMe()
+        {
+            return View();
+        }
+
+        public ActionResult slidePicture()
+        {
+            return View();
+        }
+
+        public ActionResult dogForSale()
+        {
+            return View();
+        } 
+    }
+}
--- html5-javascript-animation/TheAnimation/TheAnimation/Global.asax.cs
+++ html5-javascript-animation/TheAnimation/TheAnimation/Global.asax.cs
@@ -26,7 +26,6 @@ namespace TheAnimation
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
